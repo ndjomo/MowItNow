de677842199ac44a707515f2fb62f3eab1f13ae8
Your Name Comes Here <gaiavegit@gmail.com>
2021-12-04 18:28:31 +0100

    Exception management

---
+++ src/main/java/publicis/exercice/ndjomo/tondeuse/exception/BuisnessException.java
@@ -0,0 +1,10 @@
+package publicis.exercice.ndjomo.tondeuse.exception;
+
+/**
+ * Gestionaire d'exception par la tondeuse
+ */
+public class BuisnessException extends Exception {
+    public BuisnessException(String message) {
+        super(message);
+    }
+}
